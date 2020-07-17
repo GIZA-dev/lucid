caf90e21c4552f8b4f9f48117a70dd655012d1c2
GIZA-dev <68414316+GIZA-dev@users.noreply.github.com>
2020-07-17 12:19:16 +0600

    Add files via upload

---
+++ js/gmaps.js
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <!--- META TAGS --------------------------------------->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
+    <title>TITLE</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- CSS ---------------------------------------------->
+    <!-- Bootstrap CSS ------------------------------------>
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/bootstrap.min.css">
+    <!-- MAIN STYLES -------------------------------------->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!-- FAVICON ------------------------------------------>   
+    <link rel="icon" type="image/png" href="img/icon.png">
+
+  </head>  
+  <body>      
+    <!--========== BEGIN HEADER ==========-->
+        <header>
+            <div class="row">
+                <div class="logo">
+                    <a href="index.html">Lhander</a>
+                </div>
+                <nav id="main-nav-wrap">
+                    <ul class="main-navigation">
+                        <li class="current"><a class="smoothscroll"  href="#intro" title="">Главная</a></li>
+                        <li><a class="smoothscroll"  href="#process" title="">О компании</a></li>
+                        <li><a class="smoothscroll"  href="#features" title="">Услуги</a></li>
+                        <li><a class="smoothscroll"  href="#pricing" title="">Партнеры</a></li>
+                        <li><a class="smoothscroll"  href="#faq" title="">Что-то еще</a></li>                                 
+                    </ul>
+                </nav>
+
+                <a class="menu-toggle" href="#"><span>Menu</span></a>        
+            </div>    
+   </header> 
+   <!--========== END  HEADER ==========-->
+
+
+   <!--========== BEGIN MAIN ==========-->
+
+    <!--========== END MAIN ==========-->
+    
+
+    <!-- SCRIPT TAGS ==================================== -->
+    <!-- jQuery --------------------------------------------------->
+    
+    <!-- /jQuery -------------------------------------------------->
+
+    <!-- Popper --------------------------------------------------->
+    
+    <!-- /Popper -------------------------------------------------->
+
+    <!-- Bootstrap ------------------------------------------------>
+    
+    <!-- /Bootstrap ----------------------------------------------->
+
+    <!-- Main js -------------------------------------------------->
+
+    <!-- /Main js ------------------------------------------------->
+
+    <script
+  src="http://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
